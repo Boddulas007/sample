d3d9645c39edad7e489fac50411423beee453ce8
sai <sai.krishhna997@gmail.com>
2023-03-29 21:52:39 +0530

    for testng project

---
+++ Testng/src/test/java/com/demo/Testng/AppTest.java
@@ -0,0 +1,28 @@
+package com.demo.Testng;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void Logintest() {
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\$@i\\Desktop\\chrome driver win32\\chromedriver_win32\\chromedriver.exe");
+	  
+	  // Instantiate a ChromeDriver class.     
+	  WebDriver driver=new ChromeDriver();  
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	//Maximize the browser  
+      driver.manage().window().maximize();  
+      driver.findElement(By.name("username")).sendKeys("Admin");
+      driver.findElement(By.name("password")).sendKeys("admin123");
+     
+	  
+	  
+  }
+  }
+
---
+++ Testng/src/test/java/com/demo/Testng/Datadependencyclass.java
@@ -0,0 +1,5 @@
+package com.demo.Testng;
+
+public class Datadependencyclass {
+
+}
