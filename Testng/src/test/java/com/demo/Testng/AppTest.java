6db5d856f5133a3a7879630d506c70caf4fd5f16
sai <sai.krishhna997@gmail.com>
2023-03-30 19:28:24 +0530

    ok

--- Testng/src/test/java/com/demo/Testng/AppTest.java
+++ Testng/src/test/java/com/demo/Testng/AppTest.java
@@ -14,32 +14,22 @@ import java.util.Set;
 
 public class AppTest {
 	
- 
-  @Test
-  public void handler() {
-  System.setProperty("webdriver.chrome.driver","C:\\Users\\$@i\\Desktop\\chrome driver win32\\chromedriver1_win32\\chromedriver.exe");
-  WebDriver driver=new ChromeDriver(); 
+
+	 @Test
+	  public void Logintest() throws InterruptedException {
+		  System.setProperty("webdriver.chrome.driver","C:\\Users\\$@i\\Desktop\\chrome driver win32\\chromedriver1_win32\\chromedriver.exe");
+		  
+		  // Instantiate a ChromeDriver class.     
+		  WebDriver driver=new ChromeDriver();  
+		  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		//Maximize the browser  
+	      driver.manage().window().maximize();  
+	      
+	      Thread.sleep(6000);
+	      driver.findElement(By.name("username")).sendKeys("Admin");
+	      driver.findElement(By.name("password")).sendKeys("admin123");
+	      driver.quit();    }
   
-  driver.manage().window().maximize();
-  driver.get("https://demoqa.com/browser-windows");
-
-  // Open new child window within the main window
-  driver.findElement(By.id("windowButton")).click();
-
-  //Get handles of the windows
-  String mainWindowHandle = driver.getWindowHandle();
-  Set<String> allWindowHandles = driver.getWindowHandles();
-  Iterator<String> iterator = allWindowHandles.iterator();
-
-  // Here we will check if child window has other child windows and will fetch the heading of the child window
-  while (iterator.hasNext()) {
-      String ChildWindow = iterator.next();
-          if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
-          driver.switchTo().window(ChildWindow);
-          WebElement text = driver.findElement(By.id("sampleHeading"));
-          System.out.println("Heading of child window is " + text.getText());
-      }
-     } 
   
-  }}
+ }
 
